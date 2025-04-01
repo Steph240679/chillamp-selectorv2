c557358b8cab6cf70d2d9d868529aa11cde65a38
Steph240679 <stephane240679@gmail.com>
2025-04-01 15:53:02 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,4 +34,7 @@ def index():
     return send_from_directory(os.path.join(os.path.dirname(__file__), "../frontend"), "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+  
