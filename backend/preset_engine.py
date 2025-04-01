768762e4ab71b1558aab860070203a1f86ff8ffb
Steph240679 <stephane240679@gmail.com>
2025-04-01 15:46:14 +0200

    Update preset_engine.py

--- backend/preset_engine.py
+++ backend/preset_engine.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+
 from base_bassistes import base_bassistes
 from adaptateurs import (
     adapter_basse,
