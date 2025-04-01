272a14a9f7f0a59f240493d4afe8d8bfb9483d9b
Steph240679 <stephane240679@gmail.com>
2025-04-01 17:29:39 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask, request, jsonify, send_file, send_from_directory
 from preset_engine import get_presets_for_combination
 from pdf_generator import generate_preset_pdf_flask
+from base_bassistes import base_bassistes
+from liste_basses_completes import basses_uniques
+from liste_amplis_basse_complets import amplis_basse
+from liste_effets_basse_complets import effets_basse
+from liste_baffles_basse_complets import baffles_basse
 
 app = Flask(__name__)
 
